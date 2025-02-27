81af5e12569b23aed447c3a596cc1106a0bf2aaf
IrannaEcosmos <iranna@ecosmosolutions.com>
2025-02-27 10:15:56 +0530

    Initial commit

---
+++ src/main/java/com/example/metrocard_backend/MetrocardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.metrocard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrocardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrocardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/metrocard_backend/MetrocardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metrocard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrocardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
